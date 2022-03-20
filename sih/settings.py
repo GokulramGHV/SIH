69451b4b8c6ae8ad2179778f7ada292b89ed3cf4
Gokulram <gokulram2010418@ssn.edu.in>
2022-03-20 22:20:26 +0530

    edited settings.py

--- sih/settings.py
+++ sih/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
+import os
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, "static")
 
